d5b823a0f1ed980a89d3d266937f0b3170ecdda3
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-10-04 17:53:17 +0200

    migrating from ASP.Net MVC 5 to ASP.Net Core MVC

--- Source/FluentMetadata.MVC/FluentMetadataProvider.cs
+++ Source/FluentMetadata.MVC/FluentMetadataProvider.cs
@@ -1,57 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web.Mvc;
-
 namespace FluentMetadata.MVC
 {
-    /// <summary>A custom metadata provider for FluentMetadata.</summary>
-    public class FluentMetadataProvider : ModelMetadataProvider
+    using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+    public class FluentMetadataProvider : IDisplayMetadataProvider, IValidationMetadataProvider
     {
-        /// <summary>
-        /// Gets a <see cref="T:System.Web.Mvc.ModelMetadata"/> object for each property of a model.
-        /// </summary>
-        /// <param name="container">The container.</param>
-        /// <param name="containerType">The type of the container.</param>
-        /// <returns>
-        /// A <see cref="T:System.Web.Mvc.ModelMetadata"/> object for each property of a model.
-        /// </returns>
-        public override IEnumerable<ModelMetadata> GetMetadataForProperties(object container, Type containerType)
+        public void CreateDisplayMetadata(DisplayMetadataProviderContext context)
         {
-            var publicProps = containerType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var propertyName = context.Key.Name;
+            var containerType = context.Key.ContainerType;
+            var modelType = context.Key.ModelType;
 
-            foreach (var propertyMetadata in QueryFluentMetadata.GetMetadataFor(containerType).Properties)
+            // Pull Fluent metadata
+            var fluentMetadata = propertyName == null
+                ? QueryFluentMetadata.GetMetadataFor(modelType)
+                : QueryFluentMetadata.GetMetadataFor(containerType, propertyName);
+
+            if (fluentMetadata != null)
             {
-                var props = publicProps.Where(p => p.Name == propertyMetadata.ModelName).ToArray();
-                var prop = props.Length > 1 ? props.GetHiding() : props.SingleOrDefault();
-
-                if (prop != null)
-                {
-                    yield return new FluentModelMetadata(propertyMetadata, this, () => prop.GetValue(container, null));
-                }
+                // Example: set display name & description
+                context.DisplayMetadata.DisplayName = () => fluentMetadata.DisplayName;
+                context.DisplayMetadata.Description = () => fluentMetadata.Description;
             }
         }
 
-        /// <summary>
-        /// Gets metadata for the specified property.
-        /// </summary>
-        /// <param name="modelAccessor">The model accessor.</param>
-        /// <param name="containerType">The type of the container.</param>
-        /// <param name="propertyName">The property to get the metadata model for.</param>
-        /// <returns>
-        /// A <see cref="T:System.Web.Mvc.ModelMetadata"/> object for the property.
-        /// </returns>
-        public override ModelMetadata GetMetadataForProperty(Func<object> modelAccessor, Type containerType, string propertyName) => new FluentModelMetadata(QueryFluentMetadata.GetMetadataFor(containerType, propertyName), this, modelAccessor);
+        public void CreateValidationMetadata(ValidationMetadataProviderContext context)
+        {
+            var propertyName = context.Key.Name;
+            var containerType = context.Key.ContainerType;
+            var modelType = context.Key.ModelType;
 
-        /// <summary>
-        /// Gets metadata for the specified model accessor and model type.
-        /// </summary>
-        /// <param name="modelAccessor">The model accessor.</param>
-        /// <param name="modelType">The type of the model.</param>
-        /// <returns>
-        /// A <see cref="T:System.Web.Mvc.ModelMetadata"/> object for the specified model accessor and model type.
-        /// </returns>
-        public override ModelMetadata GetMetadataForType(Func<object> modelAccessor, Type modelType) => new FluentModelMetadata(QueryFluentMetadata.GetMetadataFor(modelType), this, modelAccessor);
+            var fluentMetadata = propertyName == null
+                ? QueryFluentMetadata.GetMetadataFor(modelType)
+                : QueryFluentMetadata.GetMetadataFor(containerType, propertyName);
+
+            if (fluentMetadata != null)
+            {
+                // Example: attach Fluent validation attributes
+                foreach (var rule in fluentMetadata.Rules)
+                {
+                    context.ValidationMetadata.ValidatorMetadata.Add(rule);
+                }
+            }
+        }
     }
 }
--- Source/FluentMetadata.MVC/FluentValidationProvider.cs
+++ Source/FluentMetadata.MVC/FluentValidationProvider.cs
@@ -1,32 +1,43 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
 using FluentMetadata.Rules;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace FluentMetadata.MVC
 {
-    public class FluentValidationProvider : ModelValidatorProvider
+    public class FluentValidationProvider : IModelValidatorProvider
     {
-        public override IEnumerable<ModelValidator> GetValidators(ModelMetadata metadata, ControllerContext context)
+        public void CreateValidators(ModelValidatorProviderContext context)
         {
-            var validators = new List<ModelValidator>();
+            // In Core, ModelMetadata is accessible from context
+            var metadata = context.ModelMetadata;
 
-            if (metadata is FluentModelMetadata)
+            if (metadata is FluentModelMetadata fluentMetadata)
             {
-                var isPropertyMetadata = !string.IsNullOrEmpty(metadata.PropertyName);
-                var rules = (metadata as FluentModelMetadata).Metadata.Rules;
+                var isPropertyMetadata = metadata.ContainerType != null && metadata.PropertyName != null;
+                var rules = fluentMetadata.Metadata.Rules;
 
                 if (isPropertyMetadata)
                 {
-                    validators.AddRange(rules.Select(rule => new RuleModelValidator(rule, metadata, context)));
+                    foreach (var rule in rules)
+                    {
+                        context.Results.Add(new ValidatorItem
+                        {
+                            Validator = new RuleModelValidator(rule),
+                            IsReusable = true
+                        });
+                    }
                 }
                 else
                 {
-                    validators.AddRange(rules.Select(rule => new ClassRuleModelValidator(rule as IClassRule, metadata, context)));
+                    foreach (var rule in rules.OfType<IClassRule>())
+                    {
+                        context.Results.Add(new ValidatorItem
+                        {
+                            Validator = new ClassRuleModelValidator(rule),
+                            IsReusable = true
+                        });
+                    }
                 }
             }
-
-            return validators;
         }
     }
 }
